4df29684786337c879d0004910fa2629286aa5d8
oscar bernardini <bernardini687@gmail.com>
2021-03-01 20:56:07 +0100

    tdd: parse C instruction

--- code/utils.test.js
+++ code/utils.test.js
@@ -16,3 +16,34 @@ describe('valueOrInsert', () => {
     expect(obj.baz).toBe(3)
   })
 })
+
+// page 7 of 18
+// file:///Users/oscar/Downloads/44046b_b73759b866b249a0b3a715bf5a18f668.pdf
+
+describe('parseInstruction', () => {
+  describe('without symbols', () => {
+    xit('parses a C instruction with dest into its components', () => {
+      const components = 'AM=M-1'
+
+      expect(components).toStrictEqual({ dest: 'AM', comp: 'M-1', jump: '' })
+    })
+
+    xit('parses a C instruction with jump into its components', () => {
+      const components = 'D;JGT'
+
+      expect(components).toStrictEqual({ dest: '', comp: 'D', jump: 'JGT' })
+    })
+
+    xit('parses a C instruction with dest and whitespace into its components', () => {
+      const components = '  D = D-M '
+
+      expect(components).toStrictEqual({ dest: 'D', comp: 'D-M', jump: '' })
+    })
+
+    xit('parses a C instruction with jump and whitespace into its components', () => {
+      const components = '  0; JMP'
+
+      expect(components).toStrictEqual({ dest: '', comp: '0', jump: 'JMP' })
+    })
+  })
+})
