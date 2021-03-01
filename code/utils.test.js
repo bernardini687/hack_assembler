9b66686fa5d2d3ff7928efbcc04064a26cecfbd9
oscar bernardini <bernardini687@gmail.com>
2021-03-01 17:56:01 +0100

    refactor: utils test

--- code/utils.test.js
+++ code/utils.test.js
@@ -1,16 +1,18 @@
 const { valueOrInsert } = require('./utils')
 
-test('return the value when found', () => {
-  const obj = { foo: 1, bar: 2 }
-  const val = valueOrInsert(obj, 'foo', 3)
+describe('valueOrInsert', () => {
+  it('returns the value when found', () => {
+    const obj = { foo: 1, bar: 2 }
+    const val = valueOrInsert(obj, 'foo', 3)
 
-  expect(val).toBe(1)
-})
+    expect(val).toBe(1)
+  })
 
-test('add the value when not found', () => {
-  const obj = { foo: 1, bar: 2 }
-  const val = valueOrInsert(obj, 'baz', 3)
+  it('adds the value when not found', () => {
+    const obj = { foo: 1, bar: 2 }
+    const val = valueOrInsert(obj, 'baz', 3)
 
-  expect(val).toBe(3)
-  expect(obj.baz).toBe(3)
+    expect(val).toBe(3)
+    expect(obj.baz).toBe(3)
+  })
 })
